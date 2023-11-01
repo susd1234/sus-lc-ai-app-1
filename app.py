b49431b58ebe1dbb253b2a659000dcf433efb33f
Susan Das <susandas@gmail.com>
2023-11-01 00:31:55 +0000

    devel final commit

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
 import os
-import openai
+# import Cohere
+import cohere
 
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
-openai.api_key = os.environ['OPENAI_API_KEY']
+_ = load_dotenv(find_dotenv()) 
+cohere.api_key = os.environ['COHERE_API_KEY']
 
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
+from langchain.llms import Cohere
+from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import SequentialChain
@@ -16,48 +17,44 @@ import streamlit as st
 
 def main():
     """
-    This script uses OpenAI's GPT-3 to generate text based on user input. It prompts the user to enter a celebrity's name,
-    then uses GPT-3 to generate the celebrity's birth date and five major events that happened around that time. The
-    results are displayed in a Streamlit app.
+    This script uses Cohere to generate text based on user input. It prompts the user to enter a Famous Personality's name,
+    then find out the birth date. After that this app findout five major events that happened around that time. The
+    results are displayed on the Web App's Screen. 
     """
-    # streamlit framework
-    st.title('Personality Search Results')
-    input_text=st.text_input("Search the topic u want")
+    
+    st.title('Correlating Event Search')
+    input_text=st.text_input("Enter a Famous Personality's Name")
 
-    # Prompt Templates
-    first_input_prompt=PromptTemplate(
+    input_prompt_1=PromptTemplate(
         input_variables=['name'],
-        template="Tell me about celebrity {name}"
+        template="Find out Info Regarding the Personality {name}"
     )
 
-    # Memory
     person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
     dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
     descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
 
-    ## OPENAI LLMS
-    llm=OpenAI(temperature=0.8)
-    chain=LLMChain(
-        llm=llm,prompt=first_input_prompt,verbose=True,output_key='person',memory=person_memory)
+    llm=Cohere(temperature=0.8)
+    chain_1=LLMChain(
+        llm=llm,prompt=input_prompt_1,verbose=True,output_key='person',memory=person_memory)
 
-    # Prompt Templates
-    second_input_prompt=PromptTemplate(
+    input_prompt_2=PromptTemplate(
         input_variables=['person'],
         template="when was {person} born"
     )
 
-    chain2=LLMChain(
-        llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob',memory=dob_memory)
+    chain_2=LLMChain(
+        llm=llm,prompt=input_prompt_2,verbose=True,output_key='dob',memory=dob_memory)
 
     # Prompt Templates
-    third_input_prompt=PromptTemplate(
+    input_prompt_3=PromptTemplate(
         input_variables=['dob'],
         template="Mention 5 major events happened around {dob} in the world"
     )
 
-    chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description',memory=descr_memory)
+    chain_3=LLMChain(llm=llm,prompt=input_prompt_3,verbose=True,output_key='description',memory=descr_memory)
     parent_chain=SequentialChain(
-        chains=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
+        chains=[chain_1,chain_2,chain_3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
 
     if input_text:
         st.write(parent_chain({'name':input_text}))
